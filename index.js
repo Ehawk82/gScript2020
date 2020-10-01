cfd37301155d2dc2cb496e12036fb1ca2840de47
Jason Graziano <mrehawk82@gmail.com>
2020-09-30 23:43:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,31 +4,43 @@ var months = ['January','February','March','April','May','June','July','August',
 var dys = ["0","1st","2nd","3rd","4th","5th","6th","7th","8th","9th","10th","11th","12th","13th","14th","15th","16th","17th","18th","19th","20th","21st","22nd","23rd","24th","25th","26th","27th","28th","29th","30th","31st"];
 
 /*global functions*/
-function bySel(x) { return document.querySelector(x) };
-function byId(x) { return document.getElementById(x) };
-function byClass(x) { return document.getElementsByClassName(x) };
-function bySelAll(x) { return document.querySelectorAll(x) };
-function deleteThis(x,y) { return setTimeout(function(){ x.remove()},y)};
-function makeFull(x) {return x.className = x.className + "_full" };
-function takeFull(x) { var y,z; return y = x.className.split('_full'), z = y[0], x.className = z };
-function createEle(x) { return document.createElement(x) };
-function byTag(x, y) { if (!y || y === null) {y = 0}  return document.getElementsByTagName(x)[y] };
-function makeLock(x) { return x.className = x.className + "_locked" };
-function takeLock(x) { var y, z; return y = x.className.split("_locked"), z = y[0], x.className = z };
-function myHeight() { return screen.height };
-function myWidth() { return screen.width };
-function thisHeight(x) { return x.clientHeight };
-function thisWidth(x) { return x.clientWidth };
-function makeCollide(x) { return x.className = x.className + "_collide" };
-function makeLeft(x) {return x.className = x.className + "_left" };
-function takeLeft(x) { var y,z; return y = x.className.split('_left'), z = y[0], x.className = z };
-function makeCenter(x) {return x.className = x.className + "_center" };
-function takeCenter(x) { var y,z; return y = x.className.split('_center'), z = y[0], x.className = z };
-function makeRight(x) {return x.className = x.className + "_right" };
-function takeRight(x) { var y,z; return y = x.className.split('_right'), z = y[0], x.className = z };
-function makeFade(x) {return x.className = x.className + "_fade" };
-function takeFade(x) { var y,z; return y = x.className.split('_fade'), z = y[0], x.className = z };
-function tfs(x) { openFullscreen(body) };
+const bySel    = x => document.querySelector(x),
+      byId     = x => document.getElementById(x),
+      byClass  = x => document.getElementsByClassName(x),
+      bySelAll = x => document.querySelectorAll(x);
+
+const deleteThis = (x, y) => setTimeout( () => x.remove(), y),
+      createEle  = x      => document.createElement(x);
+
+const byTag = (x, y) => document.getElementsByTagName(x)[y || 0];
+
+const tfs = x => openFullscreen(body);
+
+const thisHeight = x =>  x.clientHeight;
+      thisWidth  = x =>  x.clientWidth;
+
+const myHeight = () => screen.height;
+      myWidth  = () => screen.width;
+
+const make = (x, where) => make(x, where);
+
+const makeCollide = x => make(x, '_collide'),
+      makeLeft    = x => make(x, '_left'),
+      makeCenter  = x => make(x, '_center'),
+      makeRight   = x => make(x, '_right'),
+      makeFade    = x => make(x, '_fade'),
+      makeFull    = x => make(x, '_full'),
+      makeLock    = x => make(x, '_locked');
+
+const take = (x, where) => x.className = x.className.split(where)[0];
+
+const takeLeft   = x => take(x, '_left'),
+      takeCenter = x => take(x, '_center'), 
+      takeRight  = x => take(x, '_right'),  
+      takeFade   = x => take(x, '_fade'),   
+      takeFull   = x => take(x, '_full');
+      takeLock   = x => take(x, '_locked');
+
 function openFullscreen(x,y) { return function() { x.disabled = true, y.disabled = false; if (document.body.requestFullscreen) {return document.body.requestFullscreen();} else if (document.body.mozRequestFullScreen) { /* Firefox */document.body.mozRequestFullScreen();} else if (document.body.webkitRequestFullscreen) { /* Chrome, Safari & Opera */document.body.webkitRequestFullscreen();} else if (document.body.msRequestFullscreen) { /* IE/Edge */document.body.msRequestFullscreen();} }}
 function closeFullScreen(x,y) { return function(){ x.disabled = true, y.disabled = false; if (document.exitFullscreen) {document.exitFullscreen();} else if (document.mozCancelFullScreen) { /* Firefox */document.mozCancelFullScreen();} else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */document.webkitExitFullscreen();} else if (document.msExitFullscreen) { /* IE/Edge */document.msExitFullscreen();} }};
 function currentTime(){ return Math.round(new Date().getTime()/1000) };
